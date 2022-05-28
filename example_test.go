8e64970eb74ecfdcc809ae8fb5c4f8c09f1b7180
wada <wada@bengo4.com>
2022-05-29 00:19:43 +0900

    adding examples for errors.Unwrap and erros.Is.

--- example_test.go
+++ example_test.go
@@ -1,6 +1,7 @@
 package erh_test
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/bengo4/erh"
@@ -9,10 +10,12 @@ import (
 func ExampleCause() {
 	err0 := fmt.Errorf("something wrong")
 	err1 := erh.Wrap(err0)
-
+	err2 := erh.Wrap(err1)
 	fmt.Println(erh.Cause(err0) == err0)
 	fmt.Println(erh.Cause(err1) == err0)
+	fmt.Println(erh.Cause(err2) == err0)
 	// Output:
+	// true
 	// true
 	// true
 }
@@ -20,14 +23,14 @@ func ExampleCause() {
 func ExampleErrorf() {
 	err := erh.Errorf("something wrong")
 	fmt.Println(err)
-	// Output: something wrong[example_test.go:21]
+	// Output: something wrong[example_test.go:24]
 }
 
 func ExampleWrap() {
 	err0 := fmt.Errorf("something wrong")
 	err1 := erh.Wrap(err0)
 	fmt.Println(err1)
-	// Output: something wrong[example_test.go:28]
+	// Output: something wrong[example_test.go:31]
 }
 
 func ExampleWrap_nil() {
@@ -40,12 +43,38 @@ func ExampleWrap_message() {
 	cause := fmt.Errorf("something wrong")
 	err := erh.Wrap(cause, "additional message")
 	fmt.Println(err)
-	// Output: additional message; something wrong[example_test.go:41]
+	// Output: additional message; something wrong[example_test.go:44]
 }
 
 func ExampleWrap_messageFormatted() {
 	err0 := fmt.Errorf("something wrong")
 	err1 := erh.Wrap(err0, "additional message, p:%d", 123)
 	fmt.Println(err1)
-	// Output: additional message, p:123; something wrong[example_test.go:48]
+	// Output: additional message, p:123; something wrong[example_test.go:51]
+}
+
+func ExampleWrap_unwrap() {
+	err0 := fmt.Errorf("something wrong")
+	err1 := erh.Wrap(err0)
+	err2 := erh.Wrap(err1)
+	fmt.Println(errors.Unwrap(err0) == nil)
+	fmt.Println(errors.Unwrap(err1) == err0)
+	fmt.Println(errors.Unwrap(err2) == err1)
+	// Output:
+	// true
+	// true
+	// true
+}
+
+func ExampleWrap_is() {
+	err0 := fmt.Errorf("something wrong")
+	err1 := erh.Wrap(err0)
+	err2 := erh.Wrap(err1)
+	fmt.Println(errors.Is(err1, err0))
+	fmt.Println(errors.Is(err2, err1))
+	fmt.Println(errors.Is(err2, err0))
+	// Output:
+	// true
+	// true
+	// true
 }
